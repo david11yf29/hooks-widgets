e6b14dc2066559bed4c3932437c0fdcbc8387501
david11yf29 <david11yf29@gmail.com>
2020-09-13 15:10:47 +0800

    updated

--- src/components/Search.js
+++ src/components/Search.js
@@ -22,10 +22,19 @@ const Search = () => {
       setResults(data.query.search);
     };
 
-    if (term) {
+    if (term && !results.length) {
       search();
+    } else {
+      const timeoutId = setTimeout(() => {
+        if (term) {
+          search();
+        }
+      }, 500)
+      return () => {
+        clearTimeout(timeoutId);
+      };
     }
-  }, [term])
+  }, [term, results.length])
 
   const renderedResults = results.map(result => {
     return (
